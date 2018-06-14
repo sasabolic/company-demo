a6606e23b5f28b7241282834fb049442c355281a
Sasa Bolic <sasa.bolic@sixhours.io>
2018-06-14 17:53:42 +0100

    Initialize repository

---
+++ src/main/java/com/example/companydemo/CompanyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.companydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyDemoApplication.class, args);
+	}
+}
