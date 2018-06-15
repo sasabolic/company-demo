b99a9844a072fa59b57ffb36740c274881a0587a
Sasa Bolic <sasa.bolic@sixhours.io>
2018-06-15 11:21:20 +0100

    Remove test entity manager

--- src/test/java/com/example/companydemo/owner/OwnerRepositoryTest.java
+++ src/test/java/com/example/companydemo/owner/OwnerRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -14,9 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class OwnerRepositoryTest {
-
-    @Autowired
-    private TestEntityManager entityManager;
 
     @Autowired
     private OwnerRepository repository;
