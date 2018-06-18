d25ea0c9c9e420ab4556d3898577b5ec64c76354
Sasa Bolic <sasa.bolic@sixhours.io>
2018-06-18 13:51:32 +0200

    Add swagger support

---
+++ src/main/java/com/example/companydemo/core/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.companydemo.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger configuration.
+ *
+ * @author Sasa Bolic
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .useDefaultResponseMessages(false)
+                .tags(new Tag("company", "All operations on company"),
+                        new Tag("owner", "Operations for returning list of owners"));
+    }
+}
