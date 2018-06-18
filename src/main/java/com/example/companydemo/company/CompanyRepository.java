bb5af85c40aef26513d08b56195b4b58f912622e
Sasa Bolic <sasa.bolic@sixhours.io>
2018-06-18 10:56:00 +0200

    Update query with left join company with owners

--- src/main/java/com/example/companydemo/company/CompanyRepository.java
+++ src/main/java/com/example/companydemo/company/CompanyRepository.java
@@ -1,12 +1,20 @@
 package com.example.companydemo.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * Repository interface to manage {@code Company} instances.
  *
  * @author Sasa Bolic
  */
+@Transactional(readOnly = true)
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
+    @Query("SELECT c FROM Company c LEFT JOIN FETCH c.owners WHERE c.id = ?1")
+    @Override
+    Optional<Company> findById(Long id);
 }
