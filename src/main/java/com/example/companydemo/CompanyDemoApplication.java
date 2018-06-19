11cb50f1d63fe87362ebdd0f7d05c9f5898930e7
Sasa Bolic <sasa.bolic@sixhours.io>
2018-06-19 14:17:22 +0200

    Add support for CORS

--- src/main/java/com/example/companydemo/CompanyDemoApplication.java
+++ src/main/java/com/example/companydemo/CompanyDemoApplication.java
@@ -2,11 +2,24 @@ package com.example.companydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CompanyDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyDemoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS");
+			}
+		};
 	}
 }
