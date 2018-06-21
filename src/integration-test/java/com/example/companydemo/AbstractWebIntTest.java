24e5da499a8ecfa82ee5ba4e8812ccbb579a3e60
Sasa Bolic <sasa.bolic@sixhours.io>
2018-06-21 11:23:23 +0200

    Remove unnecesarry property

--- src/integration-test/java/com/example/companydemo/AbstractWebIntTest.java
+++ src/integration-test/java/com/example/companydemo/AbstractWebIntTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,9 +15,6 @@ import java.util.Locale;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractWebIntTest {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     WebApplicationContext context;
